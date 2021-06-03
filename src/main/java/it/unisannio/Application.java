ab5d035b71d17c50b063b3e549d72885e12be373
woland7 <antonio.deiasio@gmail.com>
2021-06-03 22:39:22 +0200

    second commit

--- src/main/java/it/unisannio/Application.java
+++ src/main/java/it/unisannio/Application.java
@@ -20,18 +20,18 @@ public class Application {
 	// tag::route-locator[]
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		String accountEndpoint = "http://" + System.getenv("ACCOUNT_HOST") + ":8080/Account-1.0";
-		String customerEndpoint = "http://" + System.getenv("CUSTOMER_HOST") + ":8080/Customer-1.0";
+		String accountEndpoint = "http://" + System.getenv("ACCOUNT_HOST") + ":8080";
+		String customerEndpoint = "http://" + System.getenv("CUSTOMER_HOST") + ":8080";
 		System.out.println(accountEndpoint);
 		System.out.println(customerEndpoint);
 		RouteLocator rl = builder.routes()
 			.route(p -> p
 				.path("/api-v1/bank/accounts/**")
-				.filters(f -> f.rewritePath("/api-v1/bank/","/api-v1/"))
+				.filters(f -> f.rewritePath("/api-v1/bank/","/Account-1.0/api-v1/"))
 				.uri(accountEndpoint))
 			.route(p -> p
 				.path("/api-v1/bank/customers/**")
-				.filters(f -> f.rewritePath("/api-v1/bank/","/api-v1/"))
+				.filters(f -> f.rewritePath("/api-v1/bank/","/Customer-1.0/api-v1/"))
 				.uri(customerEndpoint))
 			.build();
 		return rl;
